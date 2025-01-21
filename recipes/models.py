d9047751de38e8d9bf83296fcbcd2d1defdc8398
Allan Viana Santos <allanviana15@live.com>
2025-01-21 10:10:39 -0300

    upd

--- recipes/models.py
+++ recipes/models.py
@@ -2,14 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
-
-
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -23,10 +20,12 @@ class Recipe(models.Model):
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_publish = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    Category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+        )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
     
--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -1,10 +1,17 @@
 # from inspect import signature
 from random import randint
+
 from faker import Faker
+
+
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
+
+
 fake = Faker('pt_BR')
 # print(signature(fake.random_number))
+
+
 def make_recipe():
     return {
         'id': fake.random_number(digits=2, fix_len=True),
@@ -27,6 +34,8 @@ def make_recipe():
             'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
         }
     }
+
+
 if __name__ == '__main__':
     from pprint import pprint
     pprint(make_recipe())
