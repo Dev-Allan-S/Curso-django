ae6c35f51e5e71777f45baca010261eec9f96462
Allan Viana Santos <allanviana15@live.com>
2025-01-28 11:28:56 -0300

    remove fact

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from recipes.models import Recipe
+from django.http.response import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-
+from django.db.models import Q
 def home(request):
     recipes = Recipe.objects.filter(is_published=True)
     return render(request, 'recipes/pages/home.html', context={
@@ -28,5 +29,24 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
+    })
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+    if not search_term:
+        raise Http404
+
+    recipes = Recipe.objects.filter(
+    Q(  
+        Q(title__icontains=search_term)|
+        Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render (request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term' : search_term,
+        'recipes': recipes,
     })
 
